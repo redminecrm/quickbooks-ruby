e9c8c83ff0a54cc527d689149c90436d94cb8cb3
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2016-12-02 21:40:52 +0300

    Port to 1.8.7

--- lib/quickbooks/model/base_model.rb
+++ lib/quickbooks/model/base_model.rb
@@ -9,7 +9,7 @@ module Quickbooks
       xml_convention :camelcase
 
       def initialize(attributes={})
-        attributes.each {|key, value| public_send("#{key}=", value) }
+        attributes.each {|key, value| send("#{key}=", value) }
       end
 
       # ROXML doesnt insert the namespaces into generated XML so we need to do it ourselves
@@ -54,7 +54,7 @@ module Quickbooks
       def inspect
         # it would be nice if we could inspect all the children,
         # but it's likely to blow the stack in some cases
-        "#<#{self.class} " + 
+        "#<#{self.class} " +
         "#{attributes.map{|k,v| "#{k}: #{v.nil? ? 'nil' : v.to_s }"}.join ", "}>"
       end
       class << self
